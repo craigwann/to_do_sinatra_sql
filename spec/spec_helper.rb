b439891dabbc4703bb89e14e8488358c27422fdf
Craig Wann and Eric Shull <craig+eric@>
2018-07-16 11:58:20 -0700

    database created

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,16 @@
+ENV['RACK_ENV'] = 'test'
+
 require("rspec")
 require("pg")
-require("list")
-require("task")
 require("pry")
+require("./lib/list")
+require(".lib/task")
 
-DB = PG.connect({:dbname => "to_do_test"})
 
-RSpec.configure do |config|
-  config.after(:each) do
-    DB.exec("DELETE FROM lists *;")
-    DB.exec("DELETE FROM tasks *;")
-  end
-end
+
+# RSpec.configure do |config|
+#   config.after(:each) do
+#     DB.exec("DELETE FROM lists *;")
+#     DB.exec("DELETE FROM tasks *;")
+#   end
+# end
